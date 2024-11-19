593be894e7c96d5185d470d6295609009183a5ba
julian <jdrc@protonmail.com>
2024-11-19 13:31:03 -0500

    fix matching cards bug

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let ids = []      // Only keeps img ids that are clicked for the first time
 let flipped = []  // Only keeps image names that are generated for the first time
 let all_ids = []  // Keeps all the clicked img ids
 let clicked = []  // Keeps all the generated image names
+// let lost
 
 let cards = [
     '1up', '10-coins', '20-coins', 
@@ -25,7 +26,6 @@ function switcheroo(id) {
     
     if (!ids.includes(id)) {
         image.setAttribute('src', `images/${cards[card]}.png`)
-        //image.removeAttribute('onclick')  tryna fix the clicking issue 
         flipped.push(cards[card])
         ids.push(id)
         clicked.push(cards[card])
@@ -38,32 +38,54 @@ function switcheroo(id) {
     }
     
     if (clicked.length%2 == 0) {
-        cardCheck(clicked)
-    } 
+        cardCheck(clicked, all_ids)
+    }
 }
 
-function cardCheck(clicked) {
-    for (let i=clicked.length-2; i<clicked.length-1; i+=2) {
+function cardCheck(clicked, ids) {    
+    let image
+    let id
+    let j = 0
+
+    for (let i = clicked.length-2; i < clicked.length-1; i += 2) {
         if (clicked[i] == clicked[i+1]){
             showItem(clicked[i])
+            // id = ids[i]
+            // image = document.getElementById(id)
+            // image.removeAttribute('onclick')
+            // id = ids[i + 1]
+            // image = document.getElementById(id)
+            // image.removeAttribute('onclick')
+            // prevents matching cards to trigger onclick function
+            while (j < 2) {
+                id = ids[i + j]
+                image = document.getElementById(id)
+                image.removeAttribute('onclick')
+                j++
+            }
+            console.log(clicked[i])
         } else {
             setTimeout(softReset, 500, all_ids)
             miss++
         }
     }
-    if (miss == 3) setTimeout(youLose, 400)
+    if (miss == 3) {
+        // lost = true
+        setTimeout(youLose, 400)
+    }
 }
 
 function showItem(card) {
     let items = document.getElementById('items-container')
     let item = document.createElement('img')
+
     item.setAttribute('src', `images/item-${card}.png`)
     item.setAttribute('class', 'item')
     items.appendChild(item)
 } 
 
 function softReset(ids) {
-    for (let i=ids.length-2; i<=ids.length-1; i++) {
+    for (let i = ids.length-2; i <= ids.length-1; i++) {
         let card = document.getElementById(ids[i])
         card.setAttribute('src', 'images/ace-of-spade.png')
     }
@@ -73,6 +95,7 @@ function youLose() {
     let msg_box = document.getElementById('cards-container')
     let message = document.createElement('div')
     let sorry = randomPicker(end)
+    
     message.innerHTML = `<p class="too-bad">TOO BAD!</p><p><img class="sorry" src="images/${end[sorry]}.png"></p>`
     message.setAttribute('id', 'message')
     msg_box.innerHTML = ''
@@ -86,14 +109,9 @@ function hardReset() {
     let message = document.getElementById('message')
     let items = document.getElementById('items-container')
     let cards = document.getElementById('cards-container')
-    // let cards = document.getElementById('cards')
-    // let cards = document.createElement('div')
-    // cards.setAttribute('id', 'cards')
-    let br
-    console.log(cards)
 
     if (cards.childNodes.length == 1) {
-        for (let i=1; i<=18; i++) {
+        for (let i = 1; i <= 18; i++) {
             let card = document.createElement('img')
             let newline = document.createTextNode('\n      ')
             card.setAttribute('src', 'images/ace-of-spade.png')
@@ -101,20 +119,13 @@ function hardReset() {
             card.setAttribute('id', i)
             card.setAttribute('onclick', 'switcheroo(this.id)')
             cards.appendChild(card)
-            // cards.appendChild(newline)
-            // if (i==6 || i==12) {
-            //     br = document.createElement('br')
-            //     cards.appendChild(br)
-            // }
         }
     } else {
         cards = cards.children
-        for (i=0; i<cards.length; i++) {
+        for (i = 0; i < cards.length; i++) {
             cards[i].setAttribute('src', 'images/ace-of-spade.png')
         }
     }
-
-    // cards_container.append(cards)
     
     ids = []
     all_ids = []
@@ -123,7 +134,9 @@ function hardReset() {
     match = 0
     miss = 0
     items.innerHTML = ''
-    cards.removeChild(message)
-    cards.style.display = 'grid'
+    if (message) {
+        cards.removeChild(message)
+        cards.style.display = 'grid'
+    }
     console.clear()
 }
